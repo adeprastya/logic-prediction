189e726fabab5de39990395954afe3d867672d37
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2024-06-05 18:17:48 +0700

    fix: deploy 3

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
 	{
-		path: "/",
+		path: "/logic-prediction",
 		element: <App />
 	}
 ]);
